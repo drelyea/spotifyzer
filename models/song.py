0fb8fbd8920d49acdbfb7b8f1f6d4a3267d2d0de
drelyea <drelyea14@gmail.com>
2017-08-06 12:12:10 -0500

    simplified models

--- models/song.py
+++ models/song.py
@@ -12,12 +12,14 @@ class Song:
         self.name = track["name"]
         self.popularity = track["popularity"]
         self.duration = track["duration_ms"]
-        self.artists = track["artists"]
-        self.album = track["album"]
+        self.artist = track["album"]["artists"][0]["name"]
+        self.album = track["album"]["name"]
         self.song_id = track["id"]
         self.song_uri = track["uri"]
 
         self.id = self.song_id + self.local_timestamp.strftime("%H%M%S%d%m%Y")
 
     def __repr__(self):
-        return str.format("{}|{}|{}|", self.id, self.name, self.local_timestamp.strftime("%H:%M:%S %d-%m-%Y"))
+        return str.format("{}|{}|{}|{}|{}|{}|{}",
+                          self.id, self.name, self.local_timestamp.strftime("%H:%M:%S %d-%m-%Y"),
+                          self.artist, self.album, self.popularity, self.duration)
